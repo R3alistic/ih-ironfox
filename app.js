15c6e8280871a782f420219a2cb6027ff02b11f6
Rian Yan <rianyan9723@gmail.com>
2022-12-03 16:08:02 +0000

    new version

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 require("./config")(app);
 
 const capitalize = require("./utils/capitalize");
-const projectName = "ih-kitsune";
+const projectName = "ih-ironfox";
 
 app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 
