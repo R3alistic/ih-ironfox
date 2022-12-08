98d423b59fc549c746f773d8b4c46f1e15e40d5c
R3alistic <acunhax@gmail.com>
2022-12-08 20:59:13 +0000

    "50 animes instead of 10"

--- services/api.service.js
+++ services/api.service.js
@@ -1,10 +1,28 @@
-const kitsu = require('kitsu');
+const axios = require('axios');
+const { off } = require('../models/Anime.model');
+
 
 class ApiService {
     constructor() {
-        this.api = kitsu.create({
-            baseURL:'https://kitsu.io/api/edge'
+        this.api = axios.create({
+            baseURL:'https://kitsu.io/api/edge',
+            headers: { "Accept-Encoding": "*" } 
         });
+    }
+    getTop30 = async () => {
+        const number = 5;
+        const allData = []
+        let res = '';
+
+        for(let i = 0; i < number; i++) {
+            if(i === 0) {
+                 res = await this.api.get('/anime');
+            } else {
+                res = await this.api.get(res.data.links.next);
+            }
+            allData.push(...res.data.data)
+        }
+    return allData
     }
 }
 
